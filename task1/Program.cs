497b14a1c9b2ee415e72478180752fd83649d9bc
unknown <Dmitiy_yar@mail.ru>
2022-10-24 09:00:14 +0300

    Add task1

---
+++ task1/Program.cs
@@ -0,0 +1,29 @@
+﻿string[] array = { "hello", "2", "world", ":-)" };
+int size = array.Length;
+int number = 0;
+int sizeArrayResult = 0;
+for (int i = 0; i < size; i++)
+{
+    if (array[i].Length < 4) sizeArrayResult++;
+}
+string[] arrayResult = new string[sizeArrayResult];
+for (int i = 0; i < size; i++)
+{
+    if (array[i].Length < 4)
+    {
+        arrayResult[number] = array[i];
+        number++;
+    }
+}
+PrintArray(arrayResult);
+void PrintArray(string[] array)
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i]);
+        if (i < array.Length - 1) Console.Write(",");
+    }
+    Console.Write("]");
+}
+
